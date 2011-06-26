a03ca95219e5a7378ab7276a3bbba9af27818ef1
A.K.M. Ashrafuzzaman <jitu@dragon.cust.tele2.se>
2011-06-26 07:18:13 -0400

    Added default attributes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,9 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :profile_attributes, :join_table => "profiles_attributes"
   validates_uniqueness_of :name, :scope => :user_id
+  after_create :create_default_attributes
+  
+  def create_default_attributes
+    self.profile_attributes << ProfileAttribute.profile_attributes
+  end
 end
