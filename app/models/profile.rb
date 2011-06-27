2c2130ce71e939b6636c9f82adfea8f0d308a665
A.K.M. Ashrafuzzaman <jitu@dragon.cust.tele2.se>
2011-06-27 17:14:01 -0400

    Fixed a bug

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,10 +2,5 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :profile_attributes, :join_table => "profiles_attributes"
   validates_uniqueness_of :name, :scope => :user_id
-  after_create :create_default_attributes
-  
-  def create_default_attributes
-    self.profile_attributes << ProfileAttribute.default_attributes
-    self.user.profile_attributes << self.profile_attributes
-  end
+
 end
