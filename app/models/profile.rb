60e8beb42909d8390354587b10dbce92bebcba77
unknown <jitu@.(none)>
2011-07-17 12:05:13 +0600

    Fixed a bug

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :profile_attributes, :join_table => "profiles_attributes"
-  has_and_belongs_to_many :friends, :through => :profiles_friends 
+  has_and_belongs_to_many :friends, :join_table => :profiles_friends 
 
   validates_uniqueness_of :name, :scope => :user_id
 
