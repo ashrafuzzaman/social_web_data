0e0f8879e501f9d8eab923570837ca2b02dae445
unknown <jitu@.(none)>
2011-08-10 17:25:12 +0600

    view my comments of my status

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate_with_token
+  before_filter :authenticate_with_token, :except => [:comments_of_friend]
+  before_filter :authenticate_friend, :only => [:comments_of_friend]
+
   def index
-    @friend = Friend.find_by_email(params[:friends_email])
+    friend = Friend.find_by_email(params[:friends_email])
+    conditions = {
+      :resource_type => params[:resource_type],
+      :resource_id => params[:resource_id],
+      :user_id => current_user.id
+    }    
+    conditions.merge!({:friend_id => friend.id}) if friend
+
+    comments = Comment.find(:all, :conditions => conditions)
+
+    render :json => {:comments => comments}
+  end
+
+  def comments_of_friend
     comments = Comment.find(:all, :conditions => {
       :resource_type => params[:resource_type],
       :resource_id => params[:resource_id],
@@ -13,13 +28,14 @@ class CommentsController < ApplicationController
   end
 
   def post_comment
-    comment = Comment.create(:resource_type => params[:resource_type], :resource_id => params[:resource_id], :comment => params[:comment], :user_id => current_user.id )
+    comment_hash = {:resource_type => params[:resource_type], :resource_id => params[:resource_id], :comment => params[:comment], :user_id => current_user.id }
+    if params[:friends_email]
+      friend = Friend.find_by_email(params[:friends_email])
+      comment_hash.merge!({:friend_id => friend.id})
+    end
+
+    comment = Comment.create(comment_hash)
     render :json => comment
   end
 
-  def post_comment_on_friend
-    @friend = Friend.find_by_email(params[:friends_email])
-    comment = Comment.create(:resource_type => params[:resource_type], :resource_id => params[:resource_id], :comment => params[:comment], :friend_id => @friend.id, :user_id => current_user.id )
-    render :json => comment
-  end
 end
