5c6ff12b6568934ff6771d6f1bbc0c4e71e334f8
unknown <jitu@.(none)>
2011-09-29 18:03:32 +0200

    Added predefined attributes from the Portable Contacts http://portablecontacts.net/

--- app/models/profile_attribute.rb
+++ app/models/profile_attribute.rb
@@ -2,23 +2,32 @@ class ProfileAttribute < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :profiles, :join_table => "profiles_attributes"
   has_many :profiles_attributes, :class_name => "ProfilesAttribute"
-  
-  attr_accessor :selected
 
+  attr_accessor :selected
   #scope :attributes_by_profiles, lambda {|profile_ids| joins(:profiles_attributes).where('profile_id in (?)', profile_ids).select("DISTINCT profiles_attributes.profile_attribute_id, profile_attributes.*") }
-  
   def self.default_attributes
+
+    attribute_list = [["Display name", "string"],
+      ["Name", "string"],
+      ["Nickname", "string"],
+      ["Published", "date"],
+      ["Updated", "date"],
+      ["Birthday", "date"],
+      ["Anniversary", "date"],
+      ["Gender", "string"],
+      ["Email", "string"],
+      ["Phone", "string"],
+      ["Address", "string"],
+      ["Current city", "string"]
+    ]
+
     profile_attributes = []
-    profile_attributes << ProfileAttribute.create(:name => "First name", :attribute_type => "string")
-    profile_attributes << ProfileAttribute.create(:name => "Last name", :attribute_type => "string")
-    profile_attributes << ProfileAttribute.create(:name => "Sex", :attribute_type => "string")
-    profile_attributes << ProfileAttribute.create(:name => "Phone", :attribute_type => "string")
-    profile_attributes << ProfileAttribute.create(:name => "email", :attribute_type => "string")
-    profile_attributes << ProfileAttribute.create(:name => "Born on", :attribute_type => "date")
-    profile_attributes << ProfileAttribute.create(:name => "Current city", :attribute_type => "date")
+    attribute_list.each do |profile_attribute|
+      profile_attributes << ProfileAttribute.create(:name => profile_attribute[0], :attribute_type => profile_attribute[1])
+    end
     profile_attributes
   end
-  
+
   def self.attributes_by_profiles(profile_ids)
     find(:all, :joins => [:profiles_attributes], :conditions => [ "profile_id in (?)", profile_ids])
   end
