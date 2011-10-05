fc683f81da4031adc4900ba221aef0a93e928991
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2011-10-05 22:13:34 +0200

    Cleared database when deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
 
   def clear
     User.delete_all
+    Comment.delete_all
+    Friend.delete_all
+    ProfileAttribute.delete_all
+    Profile.delete_all
+    Status.delete_all
+    ActiveRecord::Base.connection.execute("DELETE from profiles_attributes")
+    ActiveRecord::Base.connection.execute("DELETE from profiles_friends")
+    #Profile.find_by_sql("Select * from profiles_friends")
     redirect_to "/"
   end
 
